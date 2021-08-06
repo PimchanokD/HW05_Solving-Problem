c7a3bdcb06da55acd18ad256ed591e852d9b2c47
DESKTOP-72LTF07\User <nui.pimcha@gmail.com>
2021-08-06 16:40:10 +0700

    import library

---
+++ HW05 Solving Problem.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  6 16:04:21 2021
+
+@author: User
+"""
+
+import numpy as np
+from scipy.linalg import solve
+
+A = np.array([[0.5, 0.2], [1., 1.]])
+b = np.array([10., 30.])
+x = solve(A,b)
+print('x1, x2 = ', x)
